4dc99b09f72ed9ac6655d96cb2f1ab0c4b4dd7eb
Matheus-Silas97 <matheus.silas123@gmail.com>
2020-01-09 16:20:53 -0300

    Initial commit

---
+++ app/src/main/java/com/matheussilas/calculadoradegorjetas/MainActivity.java
@@ -0,0 +1,74 @@
+package com.matheussilas.calculadoradegorjetas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextInputEditText editPrice;
+    private TextView textPorc;
+    private SeekBar seekPorc;
+    private TextView textGorj;
+    private TextView textTotal;
+
+    private double porc = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editPrice = findViewById(R.id.editPrice);
+        seekPorc = findViewById(R.id.seekPorc);
+        textPorc = findViewById(R.id.textPorc);
+        textGorj = findViewById(R.id.textGorj);
+        textTotal = findViewById(R.id.textTotal);
+
+        seekPorc.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+                porc = i;
+                textPorc.setText(Math.round(porc) + "%");
+                calculate();
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    public void calculate() {
+        String price = editPrice.getText().toString();
+        if (price == null || price.equals("")) {
+            Toast.makeText(getApplicationContext(), "Digite o valor da conta", Toast.LENGTH_SHORT).show();
+        } else {
+            double number = Double.parseDouble(price);
+
+            double resultGorj = number * (porc / 100);
+
+            textGorj.setText("R$ " + resultGorj);
+
+            double resultTotal = number + resultGorj;
+
+            textTotal.setText("R$ " + resultTotal);
+
+
+        }
+
+    }
+}
